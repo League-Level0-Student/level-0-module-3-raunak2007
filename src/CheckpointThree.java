76ae03f449bf3e5464ec09e92cb3dfe488785631
sepfish <40220408+sepfish@users.noreply.github.com>
2019-04-21 12:23:38 -0700

    module 3
    :smile:

---
+++ src/CheckpointThree.java
@@ -0,0 +1,14 @@
+
+public class CheckpointThree {
+  public static void main(String[] args) {
+	for(int i=0; i<500; i++) {
+		if(i%2==1) {
+			System.out.println(i);
+			
+		}
+		else {
+			System.out.println(" ");
+		}
+	}
+}
+}
